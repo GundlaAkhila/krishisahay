1ac43f69c4cd4c2198840f0659eb2f3826559e93
GundlaAkhila <gundlaakhila46@gmail.com>
2026-02-28 10:50:25 +0530

    Delete disease_model.py

--- disease_model.py
+++
@@ -1,7 +0,0 @@
-def predict_disease(image_path):
-    # Placeholder model
-    return {
-        "disease": "Leaf Blight",
-        "treatment": "Use recommended fungicide spray.",
-        "prevention": "Avoid overwatering."
-    }
